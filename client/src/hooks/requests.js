045ae78da3147684a34057012dfe53640b54b98b
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-10-04 08:59:52 +0100

    first commit

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-// const API_URL = 'v1';
+// const API_URL = "v1";
 
 // // Load planets and return as JSON.
 // async function httpGetPlanets() {
@@ -25,7 +25,7 @@
 //       },
 //       body: JSON.stringify(launch),
 //     });
-//   } catch(err) {
+//   } catch (err) {
 //     return {
 //       ok: false,
 //     };
@@ -38,7 +38,7 @@
 //     return await fetch(`${API_URL}/launches/${id}`, {
 //       method: "delete",
 //     });
-//   } catch(err) {
+//   } catch (err) {
 //     console.log(err);
 //     return {
 //       ok: false,
@@ -46,9 +46,4 @@
 //   }
 // }
 
-// export {
-//   httpGetPlanets,
-//   httpGetLaunches,
-//   httpSubmitLaunch,
-//   httpAbortLaunch,
-// };
+// export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
