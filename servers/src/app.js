57b5078554c850089f3e16f165fc98fa223cdaf5
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-10-06 08:40:19 +0100

    Fixed cors error

--- servers/src/app.js
+++ servers/src/app.js
@@ -1,9 +1,15 @@
-const planetRouter = require("./routes/planets/planet-router");
 const express = require("express");
+const cors = require("cors");
+const planetRouter = require("./routes/planets/planet-router");
 
 const app = express();
 
 //middleware
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(planetRouter);
 
