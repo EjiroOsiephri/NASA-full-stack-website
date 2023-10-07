1d1c2fc3d60c136366eff76b607b8c96dab02724
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-10-07 08:59:07 +0100

    Working with csv data

--- servers/src/models/planets-model.js
+++ servers/src/models/planets-model.js
@@ -1,3 +1,44 @@
-const planets = [];
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
-module.exports = planets;
+const planet = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.6 &&
+    planet["koi_insol"] < 1.1 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("kepler_data.csv").pipe(
+  parse({
+    comment: "#",
+    columns: true,
+  })
+);
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      planet.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(`We have ${planet.length} habitable planet`);
+  });
+
+module.exports = {
+  planet,
+};
