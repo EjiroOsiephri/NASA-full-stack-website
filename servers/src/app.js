bfcd615a8a1cb591800211c01be55c3bc1499df3
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-10-08 09:25:55 +0100

    Working with npm prefix

--- servers/src/app.js
+++ servers/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const planetRouter = require("./routes/planets/planet-router");
 
 const app = express();
@@ -11,6 +12,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "public", "index.html"));
+});
+
 app.use(planetRouter);
 
 module.exports = app;
