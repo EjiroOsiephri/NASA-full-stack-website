eae1f90a0b18965dd9406afb2fdd4a38122e4453
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-10-10 08:17:55 +0100

    Using hhtp request log handler with morgan

--- servers/src/app.js
+++ servers/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const planetRouter = require("./routes/planets/planet-router");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.get("/", (req, res) => {
