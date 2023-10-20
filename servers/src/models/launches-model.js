696f126ab99ea23fc5fb90c9bc01134a2734b1de
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-10-20 10:52:56 +0100

    Finishing this project today

--- servers/src/models/launches-model.js
+++ servers/src/models/launches-model.js
@@ -1,12 +1,14 @@
 const launches = new Map();
 
+let latestLaunch = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   launchDate: new Date("December 27 2030"),
   destination: "Kepler 442 -b",
-  customer: ["nasa", "Ejiro"],
   rocket: "Rocket ss1",
+  customer: ["nasa", "Ejiro"],
   upcoming: true,
   success: true,
 };
@@ -14,9 +16,23 @@ const launch = {
 launches.set(launch.flightNumber, launch);
 
 function getAllLaunches() {
-  return Array.from(launches.values())
+  return Array.from(launches.values());
+}
+
+function addNewLaunches(launch) {
+  latestLaunch++;
+  launches.set(
+    latestLaunch,
+    Object.assign(launch, {
+      flightNumber: latestLaunch,
+      success: true,
+      upcoming: true,
+      customer: ["ztm", "NASA"],
+    })
+  );
 }
 
 module.exports = {
   getAllLaunches,
+  addNewLaunches,
 };
--- servers/src/routes/launches/launches-controller.js
+++ servers/src/routes/launches/launches-controller.js
@@ -1,9 +1,12 @@
-const {getAllLaunches } = require("../../models/launches-model");
+const {
+  getAllLaunches,
+  addNewLaunches,
+} = require("../../models/launches-model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
 }
 
 module.exports = {
- httpGetAllLaunches
+  httpGetAllLaunches,
 };
