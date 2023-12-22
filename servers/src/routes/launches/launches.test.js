ad76db6c6e8206c6e539ff25d60bf71d56ea8625
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-12-22 21:32:40 +0100

    Add: Creating a test environment using jest

---
+++ servers/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+const app = require();
+
+describe("Test GET /Launch", () => {
+  test("it respond with status 200", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /Launch", () => {
+  test("it respond with status 200", () => {});
+  test("it should catch missing required properties", () => {});
+  test("i shosuld detect invalid date", () => {});
+});
